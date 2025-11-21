b0df73cf3a1b68a2bd0c77c3f77ec11a2c036c11
Manasseh Randriamitsiry <manassehrandriamitsiry@gmail.com>
2025-11-21 20:11:35 +0300

    fix(hymns): Correct lyrics and add Rive plugin
    This commit introduces several fixes and a new dependency.
    
    Specifically, it:
    - Corrects the title of hymn 257 from "AKAIKINAO NY VARAVARANA" to "AKAIKY ANAO NY VARAVARANA" and renames the corresponding JSON file.
    - Adds proper formatting for the "FAMARANANA" (Conclusion) section in multiple hymns within `hymns_combined.json` by adding newlines and quoting the title.
    - Registers the `rive_common` plugin for Linux, macOS, and Windows platforms.

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -6,6 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   awesome_notifications
   flutter_secure_storage_linux
   open_file_linux
+  rive_common
   url_launcher_linux
 )
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -17,6 +17,7 @@ import local_auth_darwin
 import open_file_mac
 import package_info_plus
 import path_provider_foundation
+import rive_common
 import shared_preferences_foundation
 import sqflite_darwin
 import url_launcher_macos
@@ -34,6 +35,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
