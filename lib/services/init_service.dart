adb441706e13d1c6e54bf863050b3bb143c35d73
Manasseh Randriamitsiry <manassehrandriamitsiry@gmail.com>
2025-11-21 21:51:05 +0300

    refactor(audio): Centralize notification and playlist logic
    This commit refactors the audio playback and notification systems.
    
    Key changes include:
    - Moves notification channel initialization from `InitService` to `NotificationService` to centralize notification logic.
    - Implements playlist functionality (next/previous) directly within `AudioService`.
    - Removes the separate `AudioForegroundService` and integrates its logic into `AudioService` and `NotificationService` for better cohesion.
    - Updates the media player notification with improved styling and button configurations.
    - `CompactAudioPlayerWidget` now sets the playlist in `AudioService` on initialization.

--- lib/services/init_service.dart
+++ lib/services/init_service.dart
@@ -13,51 +13,11 @@ import 'package:fihirana/services/hymn_service.dart';
 import 'package:fihirana/services/local_audio_service.dart';
 import 'package:fihirana/services/notification_service.dart';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:awesome_notifications/awesome_notifications.dart';
-import 'package:flutter/material.dart';
 
 class InitService {
   static Future<void> initializeNotifications() async {
     await NotificationService.initializeNotificationChannels();
-
-    await AwesomeNotifications().initialize(
-      'resource://mipmap/ic_launcher',
-      [
-        NotificationChannel(
-          channelKey: 'announcement_channel',
-          channelName: 'Filazana',
-          channelDescription: 'Notifications for announcements',
-          defaultColor: const Color(0xFF9D50DD),
-          importance: NotificationImportance.High,
-          channelShowBadge: true,
-          enableVibration: true,
-          enableLights: true,
-          defaultPrivacy: NotificationPrivacy.Public,
-          playSound: true,
-          icon: 'resource://mipmap/ic_launcher',
-        ),
-        NotificationChannel(
-          channelKey: 'hymn_download_channel',
-          channelName: 'Maka Hira',
-          channelDescription: 'Notifications for hymn downloads and updates',
-          defaultColor: const Color(0xFF9D50DD),
-          importance: NotificationImportance.High,
-          channelShowBadge: true,
-          icon: 'resource://mipmap/ic_launcher',
-        ),
-        NotificationChannel(
-          channelKey: 'basic_channel',
-          channelName: 'Basic Notifications',
-          channelDescription: 'Basic notifications channel',
-          defaultColor: const Color(0xFF9D50DD),
-          importance: NotificationImportance.High,
-          channelShowBadge: true,
-          icon: 'resource://mipmap/ic_launcher',
-        ),
-      ],
-      debug: true,
-    );
 
     await AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
       if (!isAllowed) {
