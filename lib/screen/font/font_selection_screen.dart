2f0aafc11088e5046171b2b32fda189f40d2f585
Manasseh Randriamitsiry <manassehrandriamitsiry@gmail.com>
2025-11-22 12:49:52 +0300

    adding fonts

--- lib/screen/font/font_selection_screen.dart
+++ lib/screen/font/font_selection_screen.dart
@@ -1,14 +1,7 @@
-import 'dart:convert';
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:path_provider/path_provider.dart';
+import 'package:get/get.dart';
+import '../../controller/font_controller.dart';
 import '../../l10n/app_localizations.dart';
-
-const googleFontsAPI =
-    'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyC-ak88TTqiGpT7hyZV9hOnSQhu0n9A7hU';
 
 class FontSelectionPage extends StatefulWidget {
   const FontSelectionPage({super.key});
@@ -18,80 +11,45 @@ class FontSelectionPage extends StatefulWidget {
 }
 
 class FontSelectionPageState extends State<FontSelectionPage> {
-  List<String> fonts = [];
-
-  @override
-  void initState() {
-    super.initState();
-    fetchFonts();
-  }
-
-  Future<void> fetchFonts() async {
-    try {
-      final response = await http.get(Uri.parse(googleFontsAPI));
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
-        final items = data['items'] as List;
-        setState(() {
-          fonts = items.map((e) => e['family'].toString()).toList();
-        });
-      } else {
-        throw Exception('Failed to load fonts');
-      }
-    } catch (e) {
-      if (kDebugMode) {}
-    }
-  }
+  final FontController fontController = Get.find<FontController>();
 
   @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context)!;
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         scrolledUnderElevation: 0,
         title: Text(l10n.chooseFont),
       ),
-      body: ListView.builder(
-        itemCount: fonts.length,
-        itemBuilder: (context, index) {
-          final font = fonts[index];
-          return ListTile(
-            title: Text(font),
-            onTap: () {
-              downloadAndCacheFonts(font);
-              Navigator.pop(context, font);
-            },
-          );
-        },
-      ),
+      body: Obx(() {
+        final currentFont = fontController.currentFont.value;
+        final fonts = fontController.availableFonts;
+
+        return ListView.builder(
+          itemCount: fonts.length,
+          itemBuilder: (context, index) {
+            final fontName = fonts[index];
+            final isSelected = fontName == currentFont;
+
+            return ListTile(
+              title: Text(
+                fontName,
+                style: fontController.getFontStyle(
+                    fontName, const TextStyle(fontSize: 16)),
+              ),
+              trailing: isSelected
+                  ? Icon(Icons.check, color: Theme.of(context).primaryColor)
+                  : null,
+              onTap: () {
+                fontController.changeFont(fontName);
+                Navigator.pop(context, fontName);
+              },
+            );
+          },
+        );
+      }),
     );
-  }
-
-  Future<void> downloadAndCacheFonts(String fontName) async {
-    try {
-      final response =
-          await http.get(Uri.parse('$googleFontsAPI&family=$fontName'));
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
-        final files = data['items'][0]['files'] as Map<String, dynamic>;
-        await downloadAndSaveFont(files['regular'], '$fontName-Regular.ttf');
-        await downloadAndSaveFont(files['italic'], '$fontName-Italic.ttf');
-
-        if (kDebugMode) {}
-      } else {
-        throw Exception('Failed to fetch font details for $fontName');
-      }
-    } catch (e) {
-      if (kDebugMode) {}
-    }
-  }
-
-  Future<void> downloadAndSaveFont(String url, String filename) async {
-    final response = await http.get(Uri.parse(url));
-    final bytes = response.bodyBytes;
-    final dir = await getApplicationDocumentsDirectory();
-    final fontFile = File('${dir.path}/$filename');
-    await fontFile.writeAsBytes(bytes);
   }
 }
