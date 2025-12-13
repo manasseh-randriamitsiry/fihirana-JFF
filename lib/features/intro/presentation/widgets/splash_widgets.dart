820efc21694576102f747728c80616ff76ef471d
Manasseh Randriamitsiry <manassehrandriamitsiry@gmail.com>
2025-12-13 10:16:47 +0300

    improving app startup

--- lib/features/intro/presentation/widgets/splash_widgets.dart
+++ lib/features/intro/presentation/widgets/splash_widgets.dart
@@ -131,7 +131,7 @@ class AgreementItemWidget extends StatelessWidget {
             text,
             style: TextStyle(
               fontSize: 15,
-              color: colorController.textColor.value.withValues(alpha: 0.8),
+              color: colorController.backgroundColor.value.withValues(alpha: 0.8),
               height: 1.4,
             ),
           ),
