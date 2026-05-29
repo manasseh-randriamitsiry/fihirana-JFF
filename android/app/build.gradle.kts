dd26088e136b8b35ff8784ff69113394739335e3
manasse <manasserandriamitsiry@gmail.com>
2026-05-29 12:46:15 +0300

    build: add Play Feature Delivery dependency
    Added the `com.google.android.play:feature-delivery` library to the project dependencies to enable support for on-demand module delivery.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-measurement:21.2.0")
     implementation("com.google.android.gms:play-services-measurement-api:21.2.0")
     implementation("com.google.android.play:feature-delivery:2.1.0")
+    implementation("com.google.android.play:core:1.10.3")
 }
 
 android {
