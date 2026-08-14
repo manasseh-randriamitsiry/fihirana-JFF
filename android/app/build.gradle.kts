3b74afa4b678e2857a0d7763bc2ab7f4105d8a9a
Manasseh Randriamitsiry <70688966+manasseh-randriamitsiry@users.noreply.github.com>
2026-08-14 11:19:15 +0000

    chore: bump version to 1.0.55 [skip ci]

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = 12
-        versionName = "1.0.54"
+        versionName = "1.0.55"
         multiDexEnabled = true
     }
 
